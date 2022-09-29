113a86dd65257fa44065f728b24da60b52431a34
lucianodanert <lucianodanert@gmail.com>
2022-09-29 20:28:12 -0300

    Practica de JS

---
+++ js/holaMundo-Variables.js
@@ -0,0 +1,35 @@
+
+console.log("Hola Mundo")
+
+
+
+//Este es un comentario de una sola linea
+console.log("Texto despues del comentario")
+
+console.log("Esto es un texto de prueba")
+/* Este es un comentario de varas lineas
+delimitado por los asterisco y barra
+sigue
+sigue
+sigue
+
+*/
+
+document.write("Hola Mundo ")
+
+let numero = 25;
+
+let numero2 = 39;
+
+
+document.write(numero + numero2)
+
+let palabra = ' Rick'
+
+document.write(palabra)
+
+var numero3 = 1;
+
+let nombre ='Luciano Danert'
+
+document.write(nombre)
